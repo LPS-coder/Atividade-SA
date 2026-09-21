dd04fd22412990a1f0dcc32a3333300bdb53917a
LPS-coder <arthur_l_garcia@estudante.sesisenai.org.br>
2026-09-21 08:59:54 -0300

    feat - conexao.php

---
+++ database/conexao.php
@@ -0,0 +1,14 @@
+<?php
+
+$host = "localhost";
+$usuario = "root";
+$senha = "";
+$banco = "omnirail";
+
+$conn = new mysqli($host, $usuario, $senha, $banco);
+
+if ($conn->connect_error) {
+    die("Erro na conexão com o banco de dados.");
+}
+
+$conn->set_charset("utf8mb4");
