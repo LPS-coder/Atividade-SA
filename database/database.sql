a4a0e6ca8717af17a8c0ccbeb30219d4cd051cfc
LPS-coder <arthur_l_garcia@estudante.sesisenai.org.br>
2026-09-21 08:18:13 -0300

    fix - Erro de sintaxe no codigo .sql

--- database/database.sql
+++ database/database.sql
@@ -7,16 +7,14 @@ CREATE TABLE funcionarios (
     nome VARCHAR(100) NOT NULL,
     cpf VARCHAR(11) NOT NULL UNIQUE,
     email VARCHAR(150) NOT NULL UNIQUE,
-    senha VARCHAR(255) NOT NULL,
-
+    senha VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE trens (
-    id_trem INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+    id_trem INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
     codigo VARCHAR(20) NOT NULL UNIQUE,
-    modelo VARCHAR(100) NOT NULL,
-    
+    modelo VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE sensores (
@@ -25,7 +23,5 @@ CREATE TABLE sensores (
     localizacao VARCHAR(150) NOT NULL,
     tipo_dado VARCHAR(50) NOT NULL,
     id_trem INT NOT NULL,
-
-    FOREIGN KEY (id_trem)
-        REFERENCES trens(id_trem)
+    FOREIGN KEY (id_trem) REFERENCES trens(id_trem)
 );
